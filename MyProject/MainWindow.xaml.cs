089d212d9e78fb6a7dbd59b502ab25cdaa45bfb3
Muhammed Yavuz Yeleğen <87283177+yavuzyelegen@users.noreply.github.com>
2024-07-16 21:00:00 +0300

    -buton metodları, buton renkleri, tasarımı
    -buton metodları, buton renkleri, tasarımı

--- MyProject/MainWindow.xaml.cs
+++ MyProject/MainWindow.xaml.cs
@@ -25,11 +25,145 @@ namespace MyProject
             InitializeComponent();
         }
 
+        int _deger1 = 0;
+        int _deger2 = 0;
+        char _islem;
 
         private void rakam1Btn_Click(object sender, RoutedEventArgs e)
         {
+            if (ekranLabel.Content.ToString() == "0")
+            {
+                ekranLabel.Content = "";
+            }
+            ekranLabel.Content += "1";
+        }
 
-            ekranLabel.Content = "1";
+        private void rakam2Btn_Click(object sender, RoutedEventArgs e)
+        {
+            if (ekranLabel.Content.ToString() == "0")
+            {
+                ekranLabel.Content = "";
+            }
+            ekranLabel.Content += "2";
+        }
+
+        private void rakam3Btn_Click(object sender, RoutedEventArgs e)
+        {
+            if (ekranLabel.Content.ToString() == "0")
+            {
+                ekranLabel.Content = "";
+            }
+            ekranLabel.Content += "3";
+        }
+
+        private void rakam4Btn_Click(object sender, RoutedEventArgs e)
+        {
+            if (ekranLabel.Content.ToString() == "0")
+            {
+                ekranLabel.Content = "";
+            }
+            ekranLabel.Content += "4";
+        }
+
+        private void rakam5Btn_Click(object sender, RoutedEventArgs e)
+        {
+            if (ekranLabel.Content.ToString() == "0")
+            {
+                ekranLabel.Content = "";
+            }
+            ekranLabel.Content += "5";
+        }
+
+        private void rakam6Btn_Click(object sender, RoutedEventArgs e)
+        {
+            if (ekranLabel.Content.ToString() == "0")
+            {
+                ekranLabel.Content = "";
+            }
+            ekranLabel.Content += "6";
+        }
+
+        private void rakam7Btn_Click(object sender, RoutedEventArgs e)
+        {
+            if (ekranLabel.Content.ToString() == "0")
+            {
+                ekranLabel.Content = "";
+            }
+            ekranLabel.Content += "7";
+        }
+
+        private void rakam8Btn_Click(object sender, RoutedEventArgs e)
+        {
+            if (ekranLabel.Content.ToString() == "0")
+            {
+                ekranLabel.Content = "";
+            }
+            ekranLabel.Content += "8";
+        }
+
+        private void rakam9Btn_Click(object sender, RoutedEventArgs e)
+        {
+            if (ekranLabel.Content.ToString() == "0")
+            {
+                ekranLabel.Content = "";
+            }
+            ekranLabel.Content += "9";
+        }
+
+        private void rakam0Btn_Click(object sender, RoutedEventArgs e)
+        {
+            if (ekranLabel.Content.ToString() == "0")
+            {
+                ekranLabel.Content = "";
+            }
+            ekranLabel.Content += "0";
+        }
+
+        private void artiBtn_Click(object sender, RoutedEventArgs e)
+        {
+            _deger1 = Convert.ToInt32(ekranLabel.Content);
+            _islem = '+';
+            ekranLabel.Content = "0";
+        }
+
+        private void eksiBtn_Click(object sender, RoutedEventArgs e)
+        {
+            _deger1 = Convert.ToInt32(ekranLabel.Content);
+            _islem = '-';
+            ekranLabel.Content = "0";
+        }
+
+        private void carpiBtn_Click(object sender, RoutedEventArgs e)
+        {
+            _deger1 = Convert.ToInt32(ekranLabel.Content);
+            _islem = '*';
+            ekranLabel.Content = "0";
+        }
+
+        private void bolBtn_Click(object sender, RoutedEventArgs e)
+        {
+            _deger1 = Convert.ToInt32(ekranLabel.Content);
+            _islem = '/';
+            ekranLabel.Content = "0";
+        }
+
+        private void esittirBtn_Click(object sender, RoutedEventArgs e)
+        {
+            _deger2 = Convert.ToInt32(ekranLabel.Content);
+            if (_islem == '+') { _deger1 += _deger2; }
+            else if (_islem == '-') { _deger1 -= _deger2; }
+            else if (_islem == '*') { _deger1 *= _deger2; }
+            else if (_islem == '/') { _deger1 /= _deger2; }
+            ekranLabel.Content = _deger1;
+            _islem = ' ';
+            _deger1 = 0;
+        }
+
+        private void silBtn_Click(object sender, RoutedEventArgs e)
+        {
+            _deger1 = 0;
+            ekranLabel.Content = "0";
+            _islem = ' ';
         }
     }
 }
